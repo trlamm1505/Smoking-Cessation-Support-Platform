4ba0db9c84719d835015d7a6148883d93d4f2b17
VuHoang113 <vuhoangonepiece@gmail.com>
2025-06-20 13:17:40 +0700

    fix lỗi

--- LamDuAn - Backend/appswp/src/main/java/com/minhtriet/appswp/AppswpApplication.java
+++ LamDuAn - Backend/appswp/src/main/java/com/minhtriet/appswp/AppswpApplication.java
@@ -7,7 +7,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppswpApplication {
 
 	public static void main(String[] args) {
+<<<<<<< Updated upstream:LamDuAn - Backend/appswp/src/main/java/com/minhtriet/appswp/AppswpApplication.java
 		SpringApplication.run(AppswpApplication.class, args);
+=======
+		SpringApplication.run(SwpBackendApplication.class, args);
+		System.out.println("SWP Backend Application Started!" +
+				"" +
+				"MinhTriet");
+>>>>>>> Stashed changes:SWP-Backend-Support-BE/src/main/java/com/example/SWP_Backend/SwpBackendApplication.java
 	}
 
 }
