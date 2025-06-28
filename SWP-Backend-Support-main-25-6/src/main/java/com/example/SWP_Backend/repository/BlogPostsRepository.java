6264b8493954bd013b872023b02e7b7a11227905
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-06-28 22:54:52 +0700

    Bản sửa đổi có Thành Tích

--- SWP-Backend-Support-main-25-6/src/main/java/com/example/SWP_Backend/repository/BlogPostsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.SWP_Backend.repository;
-
-import com.example.SWP_Backend.entity.BlogPosts;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BlogPostsRepository extends JpaRepository<BlogPosts, Long> {
-    // Có thể bổ sung các phương thức tìm kiếm theo status, author,...
-}
