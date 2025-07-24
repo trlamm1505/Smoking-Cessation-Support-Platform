a4df88e7209180d68a4711711501a307c3924c98
VuHoang113 <vuhoangonepiece@gmail.com>
2025-07-24 23:33:49 +0700

    delete

--- SWP-Backend-Support-main-16-07-v1/src/main/java/com/example/SWP_Backend/dto/PostCommentEditRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.SWP_Backend.dto;
-
-import lombok.Data;
-
-@Data
-public class PostCommentEditRequest {
-    private Long userId;
-    private String content;
-}
--- SWP-Backend-Support-main-16-07-v1/src/main/java/com/example/SWP_Backend/repository/MembershipPackageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.SWP_Backend.repository;
-
-import com.example.SWP_Backend.entity.MembershipPackage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MembershipPackageRepository extends JpaRepository<MembershipPackage, Long> {
-}
