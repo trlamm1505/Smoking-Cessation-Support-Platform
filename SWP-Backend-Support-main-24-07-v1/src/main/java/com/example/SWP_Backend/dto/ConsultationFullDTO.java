0df609861a1a9160d443700b3bfac32e998d1775
LeMinhTriet79 <trietleminh7979@gmail.com>
2025-07-24 13:05:32 +0700

    ĐÃ THÊM USERID VÀ COACHID VÀO API LẤY RA TẤT CẢ CÁC LỊCH TƯ VẤN ĐÃ ĐẶT
    ĐÃ THÊM USERID VÀ COACHID VÀO API LẤY RA TẤT CẢ CÁC LỊCH TƯ VẤN ĐÃ ĐẶT

--- SWP-Backend-Support-main-24-07-v1/src/main/java/com/example/SWP_Backend/dto/ConsultationFullDTO.java
+++ SWP-Backend-Support-main-24-07-v1/src/main/java/com/example/SWP_Backend/dto/ConsultationFullDTO.java
@@ -3,6 +3,8 @@ package com.example.SWP_Backend.dto;
 import java.time.LocalDateTime;
 
 public class ConsultationFullDTO {
+    private Long userId;
+    private Long coachId;
     private Long consultationId;
     private String userFullName;
     private String coachName;
@@ -96,5 +98,21 @@ public class ConsultationFullDTO {
 
     public void setMeetingLink(String meetingLink) {
         this.meetingLink = meetingLink;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getCoachId() {
+        return coachId;
+    }
+
+    public void setCoachId(Long coachId) {
+        this.coachId = coachId;
     }
 }
